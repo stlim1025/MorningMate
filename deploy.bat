7232c55096c584974f2bed58617b8dc5df8acf10
stlim1025 <stlim1025@naver.com>
2026-01-28 22:40:32 +0900

    gradle버전 업

--- deploy.bat
+++ deploy.bat
@@ -5,12 +5,9 @@ echo ==========================================
 echo [1/5] Calculating Build Version...
 echo ==========================================
 
-:: 1. 깃 커밋 총 개수를 가져와서 BUILD_NUMBER 변수에 저장
 for /f "delims=" %%i in ('git rev-list --count HEAD') do set BUILD_NUMBER=%%i
 
-:: 2. 혹시 몰라서 +1000을 해줍니다 (기존 1보다 무조건 크게 만들기 위해)
-:: 필요 없으면 set /a BUILD_NUMBER=%BUILD_NUMBER% 만 해도 됩니다.
-set /a BUILD_NUMBER=%BUILD_NUMBER% + 1000
+set /a BUILD_NUMBER=%BUILD_NUMBER%
 
 echo Current Build Number: %BUILD_NUMBER%
 
@@ -19,9 +16,7 @@ echo ==========================================
 echo [2/5] Flutter Build Start...
 echo ==========================================
 
-:: 핵심! --build-number 옵션으로 버전을 덮어씌웁니다.
-:: --build-name 옵션을 쓰면 "1.0.0" 부분도 바꿀 수 있습니다. (예: --build-name=1.0.%BUILD_NUMBER%)
-call flutter build apk --release --build-number=%BUILD_NUMBER%
+call flutter build apk --release --build-name=1.0.%BUILD_NUMBER%
 
 if %errorlevel% neq 0 (
     echo.
@@ -57,7 +52,6 @@ echo ==========================================
 echo [4/5] Uploading to Firebase...
 echo ==========================================
 
-:: 앱 ID 확인 필수!
 call firebase appdistribution:distribute build\app\outputs\flutter-apk\app-release.apk --app "1:237548170950:android:30f5e25176a3ca41dd31c7" --groups "morningmate" --release-notes-file release_notes.txt
 
 if %errorlevel% neq 0 (
